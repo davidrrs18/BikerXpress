5b31a5bf1dda519480a8e7be75eb0fd95deeb684
Rizo2301 <dinocm2301@gmail.com>
2025-08-19 23:12:49 -0600

    prueba 1

--- script.js
+++ script.js
@@ -91,6 +91,47 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
+    //funcion correo gmail
+
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const submitButton = this.querySelector('button[type="submit"]');
+    submitButton.classList.add('loading');
+
+    const formData = new FormData(this);
+
+    fetch('/send_email', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.text())
+    .then(data => {
+        showFlashMessage('Mensaje enviado correctamente.', 'success');
+        this.reset(); // Limpia el formulario
+        submitButton.classList.remove('loading');
+    })
+    .catch(error => {
+        showFlashMessage('Hubo un error al enviar el mensaje.', 'danger');
+        console.error('Error:', error);
+        submitButton.classList.remove('loading');
+    });
+});
+
+function showFlashMessage(message, category) {
+    const flashContainer = document.getElementById('flash-messages');
+    const flashMessage = document.createElement('div');
+    flashMessage.className = `alert ${category}`;
+    flashMessage.textContent = message;
+
+    flashContainer.appendChild(flashMessage);
+
+    setTimeout(() => {
+        flashMessage.remove();
+    }, 5000);
+}
+
+
 
     // --- LÓGICA PARA LOS BOTONES DE FILTRO ---
     filterBtns.forEach(btn => {
